7a9c67a230c177fd3cbcd38bc952efb0a73dfeb5
Jordan Brauer <jordan.brauer@pepper.com>
2019-10-02 15:30:30 -0500

    remove dump statement comment

--- src/Schema/AST/DocumentAST.php
+++ src/Schema/AST/DocumentAST.php
@@ -81,7 +81,6 @@ class DocumentAST implements Serializable
             } elseif ($definition instanceof DirectiveDefinitionNode) {
                 $instance->directives[$definition->name->value] = $definition;
             } else {
-                // dump($definition);
                 throw new Exception(
                     'Unknown definition type'
                 );
